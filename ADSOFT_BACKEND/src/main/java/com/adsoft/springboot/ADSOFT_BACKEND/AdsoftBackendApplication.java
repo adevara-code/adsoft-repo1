bb81fd097e61036d283220f6dce0679375ecd44f
adevara <ashoka.devara2021@gmail.com>
2021-08-01 11:16:33 +0530

    adding ADSOFT_BACKEND first commit

---
+++ ADSOFT_BACKEND/src/main/java/com/adsoft/springboot/ADSOFT_BACKEND/AdsoftBackendApplication.java
@@ -0,0 +1,13 @@
+package com.adsoft.springboot.ADSOFT_BACKEND;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsoftBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdsoftBackendApplication.class, args);
+	}
+
+}
---
+++ ADSOFT_BACKEND/src/test/java/com/adsoft/springboot/ADSOFT_BACKEND/AdsoftBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adsoft.springboot.ADSOFT_BACKEND;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsoftBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
